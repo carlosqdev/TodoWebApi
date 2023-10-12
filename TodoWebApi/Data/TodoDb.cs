f8eb1bb11b08709ae51e8c55445bf0c304c087cc
= <=>
2023-10-12 13:39:30 -0600

    Add project files.

---
+++ TodoWebApi/Data/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoWebApi.Models;
+
+namespace TodoWebApi.Data
+{
+    public class TodoDb: DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
---
+++ TodoWebApi/Models/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoWebApi.Models
+{
+    public class Todo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+        
+        public DateTime CreatedDate { get; set;}
+        [Required]
+        public bool IsCompleted { get; set; }
+    }
+}
